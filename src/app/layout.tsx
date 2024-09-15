c82c128dd17a44463def63060a5aae3c5e973b62
isaias-alt <cascolucasisaias@gmail.com>
2024-09-15 16:36:46 -0400

    refactor: delete display property from font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,7 @@ import { Poppins } from "next/font/google";
 
 const poppins = Poppins({
   subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
-  display: "swap",
+  weight: ["400", "500", "600", "700"]
 });
 
 export const metadata: Metadata = {
