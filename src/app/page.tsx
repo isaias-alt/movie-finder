f1675dd8dfc5d04869f75f33c3ab907dbf5361cf
isaias-alt <cascolucasisaias@gmail.com>
2024-10-03 14:59:41 -0400

    better looking for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+import { Lora } from "next/font/google";
+
+const lora = Lora({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"]
+});
+
 export default function HomePage() {
   return (
-    <div>
-      <h1 className="text-4xl text-center my-10">Movie Finder</h1>
+    <div className="h-screen flex flex-col items-center mt-16">
+      <h1 className="text-6xl font-bold text-center mb-8 bg-gradient-to-r from-purple-600 to-pink-600 text-transparent bg-clip-text">
+        Movie Finder
+      </h1>
+      <p className={`text-gray-600 mb-6 ${lora.className}`}>
+        Describe your movie and I&apos;ll find the best one for you.
+      </p>
     </div>
   );
 }
